80ed9fcc142c03e563f4072f42aa5c4d1164e8e5
Anhkho2000 <94821877+Anhkho2000@users.noreply.github.com>
2022-03-09 10:16:18 +0700

    update 0.0.1

--- src/components/ProductView.jsx
+++ src/components/ProductView.jsx
@@ -1,11 +1,62 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import { withRouter } from 'react-router-dom'
+// component
+import Button from './Button'
+
+//support component
+import numberWithCommas from '../utils/numberWithCommas.js'
 
 const ProductView = props => {
 
         const product = props.product
 
         const [previewImg, setPreviewImg] = useState(product.image01)
+
+        const [descriptionExpand, setDescriptionExpand] = useState(false)
+
+        const [color, setColor] = useState(undefined)
+
+        const [size, setSize] = useState(undefined)
+
+        const [quantity, setQuantity] = useState(1)
+
+        const updateQuantity = (type) => {
+                if (type === 'plus') {
+                        setQuantity(quantity + 1)
+                } else {
+                        setQuantity(quantity - 1 < 1 ? 1 : quantity - 1)
+                }
+        }
+
+        useEffect(() => {
+                setPreviewImg(product.image01)
+                setQuantity(1)
+                setColor(undefined)
+                setSize(undefined)
+        }, [product])
+
+        const check = () =>{
+                let res = true
+
+                if(color === undefined){
+                        alert('Vui lòng chọn màu sắc!');
+                        return false;
+                }
+                if(size === undefined){
+                        alert('Vui lòng chọn kích cỡ!');
+                        return false;
+                }
+                return res
+        };
+
+        const addToCart = () => {
+                if(check()) props.history.push('/cart')
+        }
+
+        const goToCart = () => {
+                if(check()) props.history.push('/cart')
+        }
 
         return (
                 <div className="product">
@@ -21,7 +72,91 @@ const ProductView = props => {
                                 <div className="product__images__main">
                                         <img src={previewImg} alt="" />
                                 </div>
+                                <div className={`product-description ${descriptionExpand ? 'expand' : ''}`}>
+                                        <div className="product-description__title">
+                                                Chi tiết sản phẩm
+                                        </div>
+                                        <div className="product-description__content" dangerouslySetInnerHTML={{ __html: product.description }}></div>
+                                        <div className="product-description__toggle">
+                                                <Button size="sm" onClick={() => setDescriptionExpand(!descriptionExpand)}>
+                                                        {
+                                                                descriptionExpand ? 'Thu gọn' : 'Xem thêm'
+                                                        }
+                                                </Button>
+                                        </div>
+                                </div>
                         </div>
+                        <div className="product__info">
+                                <h1 className="product__info__title">{product.title}</h1>
+                                <div className="product__info__item">
+                                        <div className="product__info__item__price">
+                                                {numberWithCommas(product.price)}
+                                        </div>
+                                </div>
+                                <div className="product__info__item">
+                                        <div className="product__info__item__title">
+                                                Màu sắc
+                                        </div>
+                                        <div className="product__info__item__list">
+                                                {
+                                                        product.colors.map((item, index) => (
+                                                                <div key={index} className={`product__info__item__list__item ${color === item ? 'active' : ''}`} onClick={() => setColor(item)}>
+                                                                        <div className={`circle bg-${item}`}></div>
+                                                                </div>
+                                                        ))
+                                                }
+                                        </div>
+                                </div>
+                                <div className="product__info__item">
+                                        <div className="product__info__item__title">
+                                                Kích cỡ
+                                        </div>
+                                        <div className="product__info__item__list">
+                                                {
+                                                        product.size.map((item, index) => (
+                                                                <div key={index} className={`product__info__item__list__item ${size === item ? 'active' : ''}`} onClick={() => setSize(item)}>
+                                                                        <span className="product__info__item__list__item__size">
+                                                                                {item}
+                                                                        </span>
+                                                                </div>
+                                                        ))
+                                                }
+                                        </div>
+                                </div>
+                                <div className="product__info__item">
+                                        <div className="product__info__item__title">
+                                                Số lượng
+                                        </div>
+                                        <div className="product__info__item__quantity">
+                                                <div className="product__info__item__quantity__btn" onClick={() => updateQuantity('minus')}>
+                                                        <i className="bx bx-minus"></i>
+                                                </div>
+                                                <div className="product__info__item__quantity__input">
+                                                        {quantity}
+                                                </div>
+                                                <div className="product__info__item__quantity__btn" onClick={() => updateQuantity('plus')}>
+                                                        <i className="bx bx-plus"></i>
+                                                </div>
+                                        </div>
+                                </div>
+                                <div className="product__info__item">
+                                                <Button size="sm" onClick={() => addToCart()}>Thêm vào giỏ hàng</Button>
+                                                <Button size="sm" onClick={() => goToCart()}>Mua ngay</Button>
+                                </div>
+                        </div>
+                        <div className={`product-description mobile ${descriptionExpand ? 'expand' : ''}`}>
+                                        <div className="product-description__title">
+                                                Chi tiết sản phẩm
+                                        </div>
+                                        <div className="product-description__content" dangerouslySetInnerHTML={{ __html: product.description }}></div>
+                                        <div className="product-description__toggle">
+                                                <Button size="sm" onClick={() => setDescriptionExpand(!descriptionExpand)}>
+                                                        {
+                                                                descriptionExpand ? 'Thu gọn' : 'Xem thêm'
+                                                        }
+                                                </Button>
+                                        </div>
+                                </div>
                 </div>
         )
 }
@@ -30,4 +165,4 @@ ProductView.propTypes = {
         product: PropTypes.object.isRequired,
 }
 
-export default ProductView
+export default withRouter(ProductView)
